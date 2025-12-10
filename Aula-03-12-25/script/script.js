10d9e7898cda228d353ae4bb16992f4d7e58a411
Gabiss1 <garcezoliveira10@gmail.com>
2025-12-09 23:53:06 -0300

    últimos ajustes

--- Aula-03-12-25/script/script.js
+++ Aula-03-12-25/script/script.js
@@ -6,6 +6,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const section3 = document.querySelector("#atividade-3")
     const digitar = document.getElementById("ondeVaiDigitar")
     const aparecer = document.getElementById("ondeVaiAparecer")
+    const btnAddItem = document.getElementById("btnAddItem")
+    const inputElemento = document.getElementById('meuInput');
+    const lista = document.getElementById("lista")
 
     btnVisibilidade.addEventListener("click", ()=>{
         textoDaleste.classList.toggle('hide');
@@ -23,8 +26,32 @@ document.addEventListener("DOMContentLoaded", ()=>{
         mudaFundo.classList.toggle("fundoPadrao");
     })
 
-    digitar.addEventListener("keyup", ()=>{
-        console.log(digitar.textContent.toString())
-        aparecer.textContent = digitar.textContent.toString();
+    digitar.addEventListener("keyup", (event)=>{
+        const teclaDeCaractere = /^.$/u.test(event.key);
+        const teclaModificadora = !event.ctrlKey && !event.metaKey && !event.altKey;
+
+        if (teclaDeCaractere && teclaModificadora) {
+            aparecer.textContent += event.key.replace(/[^a-zA-Z0-9]/g, '')
+        }
+
+        if (event.key == "Backspace") {
+            aparecer.textContent = aparecer.textContent.toString().slice(0, aparecer.textContent.length-1)
+            console.log(aparecer.textContent.length-1)
+        }
+
+        if (event.key == " ") {
+            aparecer.textContent += " "
+            console.log("Espaço")
+        }
+    })
+
+    btnAddItem.addEventListener("click", ()=>{
+        const itemLista = document.createElement("li");
+        const valorDigitado = inputElemento.value;
+
+        itemLista.textContent = valorDigitado
+
+        lista.appendChild(itemLista)
+        console.log(valorDigitado)
     })
 })
